2c4150fad268f2e7e17721ad42b95c2aa0bbf762
sriniksv <srini_ksv@yahoo.co.in>
2026-01-03 17:44:20 +0000

    assignment1

---
+++ Terraform/AWS/Proj7_Assignment1/main.tf
@@ -0,0 +1,58 @@
+resource "aws_vpc" "myvpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_security_group" "sg" {
+    name = "websg"
+    vpc_id = aws_vpc.myvpc.id
+    
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id  = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "public_rtb" {
+    vpc_id = aws_vpc.myvpc.id
+    route {
+       cidr_block = "0.0.0.0/0"
+       gateway_id = aws_internet_gateway.igw.id
+    }
+}
+
+resource "aws_route_table_association" "public_subnet" {
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.public_rtb.id
+}
+
+resource "aws_instance" "myinstance" {
+    ami = var.ami_id
+    instance_type = var.ec2_type
+    key_name = "mykey_pair"
+    subnet_id = aws_security_group.sg.id
+}
---
+++ Terraform/AWS/Proj7_Assignment1/variables.tf
@@ -0,0 +1,8 @@
+variable "ami_id" {
+    description = "Declare the AMI ID"
+    default = "ami-0ecb62995f68bb549"
+}
+variable "ec2_type" {
+    description = "Instance Type"
+    default = "t3.micro"
+}
